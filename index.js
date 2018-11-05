7177714a6e081edce60e49640a37831801831162
Oliver Sturm <oliver@oliversturm.com>
2018-11-05 16:38:02 +0000

    version 0.1.0

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-// Accept a tag on the command line. Ex: wordpress:4.9-php7.2-apache
-// Retrieve list of tags for repo. Ex: wordpress
-// Find and output all tags that are considered newer than the current tag.
+const axios = require('axios');
+const { greaterTags } = require('./compare');
+
+if (process.argv.length !== 4) {
+  console.error(`Syntax: find-newer-docker-image <repository> <tag-with-version>
+  
+  The repository must be accessible on the Docker Hub.
+
+  Tag should be something like these:
+  
+    4.9.8-php7.2-apache
+    jessie-20181010`);
+
+  process.exit(1);
+}
+
+const repo = process.argv[2];
+const tag = process.argv[3];
+
+const tagUrl = `https://registry.hub.docker.com/v1/repositories/${repo}/tags`;
+
+axios
+  .get(tagUrl)
+  .then(res => {
+    if (res.status !== 200) {
+      console.error('Server error: ', `${res.statusText} (${res.status})`);
+      process.exit(3);
+    }
+
+    const tags = greaterTags(tag, res.data.map(e => e.name));
+    if (tags && tags.length) {
+      for (const t of tags) console.log(t);
+    }
+  })
+  .catch(err => {
+    console.error('Error: ', err);
+    process.exit(2);
+  });
